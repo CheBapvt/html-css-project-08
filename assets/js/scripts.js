f6f7939cabe270f2dd8cefcba330006a432ba0f6
CheBapvt <hiepngstudy1604@gmail.com>
2024-08-31 17:10:36 +0700

    Responsive phần Navbar #1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -97,6 +97,8 @@ window.addEventListener("template-loaded", calArrowPos);
  * 2. Thêm class "js-dropdown" vào class "dropdown" hiện tại
  *  nếu muốn reset lại item active khi ẩn menu
  */
+
+
 window.addEventListener("template-loaded", handleActiveMenu);
 
 function handleActiveMenu() {
@@ -130,5 +132,36 @@ function handleActiveMenu() {
 
     dropdowns.forEach((dropdown) => {
         dropdown.onmouseleave = () => init();
+    });
+}
+
+/**
+ * JS toggle
+ *
+ * Cách dùng:
+ * <button class="js-toggle" toggle-target="#box">Click</button>
+ * <div id="box">Content show/hide</div>
+ */
+
+
+window.addEventListener("template-loaded", initJsToggle);
+
+function initJsToggle() {
+    $$(".js-toggle").forEach((button) => {
+        const target = button.getAttribute("toggle-target");
+        if (!target) {
+            document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
+        }
+        button.onclick = () => {
+            if (!$(target)) {
+                return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
+            }
+            const isHidden = $(target).classList.contains("hide");
+
+            requestAnimationFrame(() => {
+                $(target).classList.toggle("hide", !isHidden);
+                $(target).classList.toggle("show", isHidden);
+            });
+        };
     });
 }
